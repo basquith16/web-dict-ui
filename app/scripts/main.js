878c59289bb2b9bf33d70a5775bcefa973b514fa
Brian M Asquith <basquithcpt@gmail.com>
2015-09-15 16:01:24 -0400

    finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,19 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // const API_ROOT = 'http://192.168.128.111:3000';
   const API_ROOT = 'http://word-dict.herokuapp.com';
 
-  var appendDefinition = function (entry) {
+  var appendDefinition = function(entry) {
     var definition = $(
       "<a href='#' class='list-group-item'>" +
       "<h4 class='list-group-item-heading'>" + entry.word + "</h4>" +
-      "<p class='list-group-item-text'>" + entry.definition + "</p></a>"
+      "<p class='list-group-item-text'>" + entry.definition +
+      "</p></a>"
     );
     $('.word-list ').append(definition)
     return definition;
   }
 
-  var handleAJAXError = function(jqXHR, textStatus, errorThrown ) {
+  var handleAJAXError = function(jqXHR, textStatus, errorThrown) {
     console.log(textStatus, errorThrown);
   };
 
@@ -22,7 +23,7 @@ $(document).ready(function () {
     });
   };
 
-  $('.add-definition-form').on('submit', function (event) {
+  $('.add-definition-form').on('submit', function(event) {
     event.preventDefault();
     $('.add-definition-modal').modal('hide');
     var entry = {
@@ -32,24 +33,33 @@ $(document).ready(function () {
     var newDefinition = appendDefinition(entry);
 
     newDefinition.addClass("highlight");
-    setTimeout(function () {
+    setTimeout(function() {
       newDefinition.removeClass("highlight");
     }, 1000);
 
     $.ajax(API_ROOT + '/create', {
       method: 'POST',
       data: entry
-    }).done(function (data) {
+    }).done(function(data) {
       newDefinition.addClass("highlight");
-      setTimeout(function () {
+      setTimeout(function() {
         newDefinition.removeClass("highlight");
       }, 1000);
     }).fail(handleAJAXError);
   })
+  const TEST_URL = 'http://word-dict.herokuapp.com/words.json';
 
-  $('.word-search-form').on('submit', function (event) {
-    console.log("Handle search here...");
-  });
+  $('.word-search-form').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax(API_ROOT + '/search', {
+      data: {
+        q: $('input[name=q]', this).val()
+      }
+    }).done(function(data) {
+      $('.word-list').empty();
+      handleWordList(data);
+    }).fail(handleAJAXError)
+  })
 
   $.ajax(API_ROOT + '/words.json')
     .done(handleWordList)
